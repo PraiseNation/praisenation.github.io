529d70da27cccfc494e7feaba65b71fcad89a4e4
abla-creator <70481342+abla-creator@users.noreply.github.com>
2021-07-22 16:17:30 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Pictures from './Components/Pictures'
+import Navbar from './Components/Navbar'
+import Images from './Components/Images'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Navbar />
+      <Images />
+    
+    </div>
+  );
+}
+
+export default App;
